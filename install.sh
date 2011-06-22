e199adf7a93c71876c61951e24743f5a8e9bf33e
Byron Gibson <fbg111@gmail.com>
2011-06-21 19:57:15 -0700

    init:install.sh,uninstall.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo cp -rf doc/ ftdetect/ ftplugin/ indent/ plugin/ slime/ swank-clojure/ syntax/ /usr/share/vim/vim72/
---
+++ uninstall.sh
@@ -0,0 +1,125 @@
+#!/bin/sh
+
+vim_home="/usr/share/vim/vim72"
+
+sudo rm $vim_home/doc/paredit.txt
+sudo rm $vim_home/doc/slimv.txt
+sudo rm $vim_home/doc/swank.txt
+
+sudo rm $vim_home/ftdetect/clojure.vim
+
+sudo rm $vim_home/ftplugin/clojure/slimv-clojure.vim
+sudo rm $vim_home/ftplugin/lisp/slimv-lisp.vim
+sudo rm $vim_home/ftplugin/scheme/slimv-scheme.vim
+sudo rm $vim_home/ftplugin/slimv-clhs.vim
+sudo rm $vim_home/ftplugin/slimv-cljapi.vim
+sudo rm $vim_home/ftplugin/slimv-javadoc.vim
+sudo rm $vim_home/ftplugin/slimv.py
+sudo rm $vim_home/ftplugin/slimv.vim
+sudo rm $vim_home/ftplugin/swank.py
+
+sudo rm $vim_home/indent/clojure.vim
+sudo rm $vim_home/indent/lisp.vim
+
+sudo rm $vim_home/plugin/paredit.vim
+
+sudo rm $vim_home/slime/ChangeLog
+sudo rm $vim_home/slime/metering.lisp
+sudo rm $vim_home/slime/nregex.lisp
+sudo rm $vim_home/slime/README
+sudo rm $vim_home/slime/sbcl-pprint-patch.lisp
+sudo rm $vim_home/slime/start-swank.lisp
+sudo rm $vim_home/slime/swank-abcl.lisp
+sudo rm $vim_home/slime/swank-allegro.lisp
+sudo rm $vim_home/slime/swank.asd
+
+sudo rm $vim_home/slime/swank-backend.lisp
+sudo rm $vim_home/slime/swank-ccl.lisp
+sudo rm $vim_home/slime/swank-clisp.lisp
+sudo rm $vim_home/slime/swank-cmucl.lisp
+sudo rm $vim_home/slime/swank-corman.lisp
+sudo rm $vim_home/slime/swank-ecl.lisp
+sudo rm $vim_home/slime/swank-gray.lisp
+sudo rm $vim_home/slime/swank.lisp
+sudo rm $vim_home/slime/swank-lispworks.lisp
+sudo rm $vim_home/slime/swank-loader.lisp
+sudo rm $vim_home/slime/swank-match.lisp
+sudo rm $vim_home/slime/swank-rpc.lisp
+sudo rm $vim_home/slime/swank-sbcl.lisp
+sudo rm $vim_home/slime/swank-scl.lisp
+sudo rm $vim_home/slime/swank-source-file-cache.lisp
+sudo rm $vim_home/slime/swank-source-path-parser.lisp
+sudo rm $vim_home/slime/xref.lisp
+
+sudo rm $vim_home/slime/contrib/README
+sudo rm $vim_home/slime/contrib/swank-arglists.lisp
+
+sudo rm $vim_home/slime/contrib/swank-asdf.lisp
+sudo rm $vim_home/slime/contrib/swank-clipboard.lisp
+sudo rm $vim_home/slime/contrib/swank-c-p-c.lisp
+sudo rm $vim_home/slime/contrib/swank-fancy-inspector.lisp
+sudo rm $vim_home/slime/contrib/swank-fuzzy.lisp
+sudo rm $vim_home/slime/contrib/swank-hyperdoc.lisp
+sudo rm $vim_home/slime/contrib/swank-indentation.lisp
+sudo rm $vim_home/slime/contrib/swank-kawa.scm
+sudo rm $vim_home/slime/contrib/swank-larceny.scm
+sudo rm $vim_home/slime/contrib/swank-listener-hooks.lisp
+sudo rm $vim_home/slime/contrib/swank-media.lisp
+sudo rm $vim_home/slime/contrib/swank-mit-scheme.scm
+sudo rm $vim_home/slime/contrib/swank-motd.lisp
+sudo rm $vim_home/slime/contrib/swank-package-fu.lisp
+sudo rm $vim_home/slime/contrib/swank-presentations.lisp
+sudo rm $vim_home/slime/contrib/swank-presentation-streams.lisp
+sudo rm $vim_home/slime/contrib/swank-r6rs.scm
+sudo rm $vim_home/slime/contrib/swank-sbcl-exts.lisp
+sudo rm $vim_home/slime/contrib/swank-snapshot.lisp
+sudo rm $vim_home/slime/contrib/swank-sprof.lisp
+
+sudo rm $vim_home/swank-clojure/COPYING
+sudo rm $vim_home/swank-clojure/project.clj
+sudo rm $vim_home/swank-clojure/README.md
+
+sudo rm $vim_home/swank-clojure/leiningen/swank.clj
+
+sudo rm $vim_home/swank-clojure/swank/commands.clj
+sudo rm $vim_home/swank-clojure/swank/core.clj
+sudo rm $vim_home/swank-clojure/swank/dev.clj
+sudo rm $vim_home/swank-clojure/swank/loader.clj
+sudo rm $vim_home/swank-clojure/swank/rpc.clj
+sudo rm $vim_home/swank-clojure/swank/swank.clj
+sudo rm $vim_home/swank-clojure/swank/util.clj
+
+sudo rm $vim_home/swank-clojure/swank/clj_contrib/macroexpand.clj
+sudo rm $vim_home/swank-clojure/swank/clj_contrib/pprint.clj
+
+sudo rm $vim_home/swank-clojure/swank/commands/basic.clj
+sudo rm $vim_home/swank-clojure/swank/commands/completion.clj
+sudo rm $vim_home/swank-clojure/swank/commands/contrib.clj
+sudo rm $vim_home/swank-clojure/swank/commands/indent.clj
+sudo rm $vim_home/swank-clojure/swank/commands/inspector.clj
+sudo rm $vim_home/swank-clojure/swank/commands/xref.clj
+
+sudo rm $vim_home/swank-clojure/swank/core/connection.clj
+sudo rm $vim_home/swank-clojure/swank/core/hooks.clj
+sudo rm $vim_home/swank-clojure/swank/core/protocol.clj
+sudo rm $vim_home/swank-clojure/swank/core/server.clj
+sudo rm $vim_home/swank-clojure/swank/core/threadmap.clj
+
+sudo rm $vim_home/swank-clojure/swank/util/class_browse.clj
+sudo rm $vim_home/swank-clojure/swank/util/clojure.clj
+sudo rm $vim_home/swank-clojure/swank/util/hooks.clj
+sudo rm $vim_home/swank-clojure/swank/util/io.clj
+sudo rm $vim_home/swank-clojure/swank/util/java.clj
+sudo rm $vim_home/swank-clojure/swank/util/string.clj
+sudo rm $vim_home/swank-clojure/swank/util/sys.clj
+
+sudo rm $vim_home/swank-clojure/swank/util/concurrent/mbox.clj
+sudo rm $vim_home/swank-clojure/swank/util/concurrent/thread.clj
+
+sudo rm $vim_home/swank-clojure/swank/util/net/sockets.clj
+
+sudo rm $vim_home/syntax/clojure/slimv-syntax-clojure.vim
+
+sudo rm $vim_home/syntax/scheme/slimv-syntax-scheme.vim
+
+
